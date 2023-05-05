00453d8eb5a13d19e22c8cb5236e20fdb51c5e74
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-05-05 16:10:18 +0300

    Added functions to work with the contact collection.

---
+++ contacts.js
@@ -0,0 +1,58 @@
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(({ id }) => id === contactId);
+  return result || null;
+}
+async function addContact(data) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(10),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+async function updateContactById(id, data) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = {
+    id,
+    ...data,
+  };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(({ id }) => id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContactById,
+  removeContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log("hello");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContactById,
+  removeContact,
+} = require("./contacts");
+
+// const argv = require("yargs").argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      return console.log(allContacts);
+    //   break;
+
+    case "get":
+      const oneContact = await getContactById(id);
+      return console.log(oneContact);
+    //   break;
+
+    case "add":
+      const newContact = await addContact({ name, email, phone });
+      return console.log(newContact);
+    //   break;
+
+    case "update":
+      const updateContact = await updateContactById(id, { name, email, phone });
+      return console.log(updateContact);
+    //   break;
+
+    case "remove":
+      const removeContactId = await removeContact(id);
+      return console.log(removeContactId);
+    //   break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "qdggE76Jtbfd9eWJHrssH" });
+// invokeAction({
+//   action: "add",
+//   name: "Stephan Bodzin",
+//   email: "stephan_bodzin@gmail.com",
+//   phone: "(987) 381-2578",
+// });
+// invokeAction({
+//   action: "update",
+//   id: "9DOq0IXq95",
+//   name: "Stephan Bodzin",
+//   email: "stephan_bodzin@gmail.com",
+//   phone: "(987) 457-7890",
+// });
+invokeAction({ action: "remove", id: "9DOq0IXq95" });
+
+// invokeAction(argv);
